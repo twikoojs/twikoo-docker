ba0bea94a1bb3f6fc9c96b13fe9974c7d948992b
iMaeGoo <mail1st@qq.com>
2021-10-19 21:01:28 +0800

    fix: support vercel response api

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const http = require('http')
+const twikoo = require('twikoo-vercel')
 const server = http.createServer()
-server.on('request', require('twikoo-vercel'))
+server.on('request', function (request, response) {
+  // 兼容 Vercel
+  response.status = function (code) {
+    this.statusCode = code
+    return this
+  }
+  response.json = function (json) {
+    this.writeHead(200, { 'Content-Type': 'application/json' })
+    this.end(JSON.stringify(json))
+    return this
+  }
+  twikoo(request, response)
+})
 server.listen(8080, function () {
   console.log('Twikoo function started on port 8080')
 })
